93aa520a071c71126dfb7e0d5cff95e8d5d29da5
Mike Vanbuskirk <mvanbuskirk@salesforce.com>
2020-01-28 09:58:38 -0500

    initial commit of emacs config

---
+++ init.el
@@ -0,0 +1,45 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(display-line-numbers-mode t)
+
+(set-frame-font "Source Code Pro-14" nil t)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package base16-theme
+  :ensure t
+  :config
+  (load-theme 'base16-materia t))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package magit
+   :ensure t)
+
+(setq custom-file "~/.emacs.d/package-selected-packages.el")
+(load custom-file)
