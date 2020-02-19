aee7289b211dc21586b2187893adf5f85cc9947b
codevbus <mvanbus43@gmail.com>
2020-02-19 10:52:20 -0500

    add config for straight pkg mgmt, new pkgs w. org, updated ignore

--- init.el
+++ init.el
@@ -1,49 +1,129 @@
-;; -*- mode: elisp -*-
+;;;; -*- lexical-binding: t -*-
+;;; Straight setup
+(setq straight-repository-branch "develop")
 
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+
+;;; Use-package Setup
+(straight-use-package 'use-package)
+(use-package blackout
+  :straight (blackout :host github :repo "raxod502/blackout")
+  :demand t)
+
+(use-package el-patch
+  :straight (:host github
+                   :repo "raxod502/el-patch"
+                   :branch "develop"))
+(eval-when-compile
+  (require 'el-patch))
+
+(use-package use-package-company
+  :straight (use-package-company :host github :repo "akirak/use-package-company"))
+
+;; Some combination of GNU TLS and Emacs fail to retrieve archive
+;; contents over https.
+;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+
+(if (and (version< emacs-version "26.3") (>= libgnutls-version 30604))
+    (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
 (setq inhibit-splash-screen t)
 
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
+;;;;Visual config
+(global-display-line-numbers-mode t)
+(set-frame-font "Source Code Pro For Powerline-14" nil t)
+
+;;;; Helm configuration
+;; Enable Helm
+(use-package helm)
+
+;; Set default M-x to Helm
+(global-set-key (kbd "M-x") 'helm-M-x)
+
 ;;;;Org mode configuration
 ;; Enable Org mode
-(require 'org)
+(use-package org)
 ;; Make Org mode work with files ending in .org
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; The above is the default in recent emacsen
 ;; Org mode workflow state
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+;; Kbd shortcuts
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c [") 'org-agenda-file-to-front)
+(global-set-key (kbd "C-c ]") 'org-remove-file)
+;; org agenda files
+(setq org-agenda-files '("~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org"))
 
-(display-line-numbers-mode t)
+(use-package org-bullets
+  :after org
+  :hook
+  (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list
+   '("◉" "○")))
 
-(set-frame-font "Source Code Pro For Powerline-14" nil t)
+;; Org roam
+(use-package org-roam
+      :after org
+      :hook 
+      ((org-mode . org-roam-mode)
+       (after-init . org-roam--build-cache-async) ;; optional!
+       )
+      :straight (:host github :repo "jethrokuan/org-roam" :branch "develop")
+      :custom
+      (org-roam-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/2b/org")
+      :bind
+      ("C-c n l" . org-roam)
+      ("C-c n t" . org-roam-today)
+      ("C-c n f" . org-roam-find-file)
+      ("C-c n i" . org-roam-insert)
+      ("C-c n g" . org-roam-show-graph))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Org journal
+(use-package org-journal
+  :bind
+  ("C-c n j" . org-journal-new-entry)
+  :custom
+  (org-journal-date-prefix "#+TITLE: ")
+  (org-journal-file-format "%Y-%m-%d.org")
+  (org-journal-dir "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/2b/org")
+  (org-journal-date-format "%A, %d %B %Y"))
+
+;; Deft
+(use-package deft
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/2b/org"))
+
+;;;;Misc config
+;; Set the exec path from the shell path
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
 (use-package base16-theme
   :ensure t
